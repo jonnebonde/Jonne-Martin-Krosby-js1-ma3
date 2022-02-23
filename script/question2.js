730c1dbf1d473aef26253b580af8d02e6606f147
jonnebonde <jonnebonde@gmail.com>
2022-02-23 11:14:07 +0100

    enter

--- script/question2.js
+++ script/question2.js
@@ -1,3 +1,5 @@
+
+
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=69c560cd78c84554953747e1790c6b33";
 
 const resultsContainer = document.querySelector(".result-container");
@@ -15,16 +17,11 @@ setTimeout(function() {
             
                 const facts = results.results;
             
-                //console.log(facts.length)
-               
-                //console.log(facts);
-
+              
                 resultsContainer.innerHTML = "" ;
             
                 for(let i = 0; i < facts.length; i++) {
-                    //console.log(facts[i].name)
-                    //console.log(facts[i].rating)
-                    //console.log(facts[i].tags)
+                  
             
                     if(i === 8) {
                         break;
@@ -35,7 +32,7 @@ setTimeout(function() {
                     const gameTags = (facts[i].tags).length;
 
                                    
-                  resultsContainer.innerHTML += `<div class="result">
+                    resultsContainer.innerHTML += `<div class="result">
                                                     <h2>Name: ${gameName}</h2>
                                                     <p>Rating: ${gameRating}</p>
                                                     <p>Number of tags: ${gameTags}</div>`
