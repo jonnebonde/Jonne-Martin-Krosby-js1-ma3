316dd794762e04ef77df8c1a521d8e1d7137a7f6
jonnebonde <jonnebonde@gmail.com>
2022-02-23 11:04:04 +0100

    enter

--- script/question2.js
+++ script/question2.js
@@ -1,6 +1,6 @@
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=69c560cd78c84554953747e1790c6b33";
 
-const resultsContainer = document.querySelector(".container");
+const resultsContainer = document.querySelector(".result-container");
 
 
 setTimeout(function() {
@@ -44,7 +44,7 @@ setTimeout(function() {
         
         catch (error) {
             console.log("error while loading API")
-            resultsContainer.innerHTML = errorMessage("The API wouldnt phone home, try to refresh page (press F5)")
+            resultsContainer.innerHTML = errorMessage("Could`n fetch the API, try to refresh page (press F5)")
         }
         
        
@@ -53,6 +53,6 @@ setTimeout(function() {
     getApi();
 
 
-}, 1500) 
+}, 1100) 
 
 
