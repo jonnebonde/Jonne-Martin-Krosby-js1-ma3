f8a986d725eea8e49a36c9570482fed90a5fd5f5
jonnebonde <jonnebonde@gmail.com>
2022-02-27 20:25:18 +0100

    enter

--- script/question2.js
+++ script/question2.js
@@ -1,6 +1,6 @@
 
 
-const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=69c560cd78c84554953747e1790c6b33";
+const ur = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=69c560cd78c84554953747e1790c6b33";
 
 const resultsContainer = document.querySelector(".result-container");
 
@@ -42,7 +42,7 @@ setTimeout(function() {
         
         catch (error) {
             console.log("error while loading API")
-            resultsContainer.innerHTML = errorMessage("Unwanted error occured")
+            resultsContainer.innerHTML = errorMessage("Unwanted error occured!!")
         }
         
        
